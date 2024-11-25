ff7219573ef6c4f8ec3462965ef29146c24cab8e
TKAkhter <talhaakhter01@gmail.com>
2024-11-25 17:51:40 +0500

    more minor linting done

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -9,45 +9,47 @@ interface RequestWithUser extends Request {
   user?: any;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const errorHandler = (
-  err: any,
-  req: RequestWithUser,
-  res: Response,
-  _: NextFunction,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    err: any,
+    req: RequestWithUser,
+    res: Response,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    _: NextFunction
 ): Response => {
-  // eslint-disable-line @typescript-eslint/no-explicit-any
-  const { message, ...details } = err;
-  const isHttpError = err instanceof HttpError;
+    const { message, ...details } = err;
+    const isHttpError = err instanceof HttpError;
 
-  const statusCode = isHttpError
-    ? err.status || StatusCodes.INTERNAL_SERVER_ERROR
-    : StatusCodes.INTERNAL_SERVER_ERROR;
-  const name = isHttpError ? err.name : "AppError";
-  const user = req.user?.email || "Unknown User";
-  const { method } = req;
-  const url = req.originalUrl;
+    const statusCode = isHttpError
+        ? err.status || StatusCodes.INTERNAL_SERVER_ERROR
+        : StatusCodes.INTERNAL_SERVER_ERROR;
+    const name = isHttpError
+        ? err.name
+        : "AppError";
+    const user = req.user?.email || "Unknown User";
+    const { method } = req;
+    const url = req.originalUrl;
 
-  const { stack } = err;
+    const { stack } = err;
 
-  const errorPayload = {
-    status: statusCode,
-    message,
-    method,
-    url,
-    user,
-    name,
-    details,
-    stack,
-  };
+    const errorPayload = {
+        "status": statusCode,
+        message,
+        method,
+        url,
+        user,
+        name,
+        details,
+        stack
+    };
 
-  logger.error(JSON.stringify(errorPayload));
+    logger.error(JSON.stringify(errorPayload));
 
-  const responsePayload = {
-    status: statusCode,
-    message,
-    ...(env.NODE_ENV !== "production" && { method, url, user, name, details, stack }),
-  };
+    const responsePayload = {
+        "status": statusCode,
+        message,
+        ...env.NODE_ENV !== "production" && { method, url, user, name, details, stack }
+    };
 
-  return res.status(statusCode).json(responsePayload);
+    return res.status(statusCode).json(responsePayload);
 };
